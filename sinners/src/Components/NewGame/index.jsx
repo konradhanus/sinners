a3b98e3893380a2e88344ec1cbdd3ab6c0fc1653
Konrad Hanus <konrad.hanus@intive.com>
2022-07-22 15:01:01 +0200

    arrow keys added

--- sinners/src/Components/NewGame/index.jsx
+++ sinners/src/Components/NewGame/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import useCanvas from './useCanvas';
 
 const gravity = 0.5;
@@ -14,7 +14,7 @@ class Player{
         
         this.velocity = {
             x:0, 
-            y:1
+            y:0
         }
 
         this.width = 30
@@ -29,6 +29,7 @@ class Player{
     update() {
         this.draw();
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
 
         if(this.position.y + this.height + this.velocity.y <= this.canvas.height)
         {
@@ -41,16 +42,93 @@ class Player{
 
 
 const NewGame = () => {
+    const [player, setPlayer] = React.useState();
+    const [ctxState, setCtxState] = React.useState();
+    const [canvasState, setCanvasState] = React.useState();
+    const [isPressedKeyLeft, setIsPressedKeyLeft] = React.useState(false);
+    const [isPressedKeyRight, setIsPressedKeyRight] = React.useState(false);
+    const handleUserKeyDownPress = useCallback(
+        ({ keyCode }) => {
+          if (keyCode === 39) {
+        //    console.log(' go right')
+           setIsPressedKeyRight(true);
+        //    player.velocity.x = 1
+          }
+          if (keyCode === 37) {
+            // console.log('go left');
+            setIsPressedKeyLeft(true);
+            //    player.velocity.x = 1
+            // player.velocity.x -= 20
+          }
+          if (keyCode === 38) {
+            console.log('jump', player);
+
+            player.velocity.y -= 20
+          }
+          if (keyCode === 40) {
+            console.log('fall')
+          }
+        },
+        [player]
+      );
+
+      const handleUserKeyUpPress = useCallback(
+        ({ keyCode }) => {
+          if (keyCode === 39) {
+           console.log(' go right');
+           setIsPressedKeyRight(false);
+        //    player.velocity.x += 0
+          }
+          if (keyCode === 37) {
+            console.log('go left');
+            setIsPressedKeyLeft(false);
+            // player.velocity.x -= 20
+          }
+          if (keyCode === 38) {
+            console.log('jump', player);
+
+            player.velocity.y -= 20
+          }
+          if (keyCode === 40) {
+            console.log('fall')
+          }
+        },
+        [player]
+      ); 
+
+    useEffect(() => {
+        // component did mount
+        window.addEventListener("keydown", handleUserKeyDownPress);
+        window.addEventListener("keyup", handleUserKeyUpPress);
     
+        // component will unmount
+        return () => {
+          window.removeEventListener("keydown", handleUserKeyDownPress);
+          window.removeEventListener("keyup", handleUserKeyUpPress);
+        };
+      }, [player]);
+
     const animate = () => {
         
         requestAnimationFrame(animate);
         ctxState && canvasState && ctxState.clearRect(0,0, canvasState.width, canvasState.height);
-        player !== undefined && player.update();
+        if(player !== undefined){
+            player.update();
+            // console.log('a',isPressedKeyRight)
+            if(isPressedKeyRight)
+            {
+                player.velocity.x = 1;
+            }else if(isPressedKeyLeft)
+            {
+                player.velocity.x = -1;
+            }else{
+                player.velocity.x = 0;
+            }
+
+        }
+
     } 
-    const [player, setPlayer] = React.useState();
-    const [ctxState, setCtxState] = React.useState();
-    const [canvasState, setCanvasState] = React.useState();
+    
     
     const canvasRef = useCanvas(([canvas, ctx]) => {
         canvas.width= window.innerWidth;
@@ -64,8 +142,7 @@ const NewGame = () => {
       });
     
     
-    animate();
-      
+    player && animate();
     return <canvas ref={canvasRef}></canvas>
 }
 
