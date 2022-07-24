163d65114a153c627c833b9eec8709bb6f439b61
Konrad Hanus <konrad.hanus@gmail.com>
2022-07-24 19:29:50 +0200

    is error

--- sinners/src/Components/Engine/index.jsx
+++ sinners/src/Components/Engine/index.jsx
@@ -83,93 +83,7 @@ const Engine = ({level}) => {
 
     let scrollOffset = 0;
 
-    const animate = () => {
-        
-        requestAnimationFrame(animate);
-        if(ctxState && canvasState)
-        {
-            ctxState.fillStyle = "white";
-            ctxState.fillRect(
-                0,
-                0, 
-                canvasState.width, 
-                canvasState.height
-              );
-        }
-
-        if(genericObjects){
-          genericObjects.forEach(genericObject => {
-            genericObject.draw();
-          })
-        }
-
-        if(player !== undefined && platforms !== undefined){
-            player.update();
-            // console.log('a',isPressedKeyRight)
-            if(keys.right.pressed && player.position.x < 400) {
-                player.velocity.x = playerSpeed;
-            } else if(keys.left.pressed && player.position.x > 100) {
-                player.velocity.x = -playerSpeed;
-            } else {
-                player.velocity.x = 0;
-
-                if(keys.right.pressed){
-                  scrollOffset += playerSpeed;
-
-                  platforms.forEach((platform)=>{
-                    platform.position.x -= playerSpeed
-                  })
-
-                  genericObjects.forEach((genericObject)=>{
-                    genericObject.position.x -= playerSpeed * 0.66
-                  })
-                 
-                } else  if(keys.left.pressed){
-
-                  platforms.forEach((platform)=>{
-                    scrollOffset -= playerSpeed;
-                    platform.position.x += playerSpeed
-                  })
-
-                  genericObjects.forEach((genericObject)=>{
-                    genericObject.position.x += playerSpeed * 0.66
-                  })
-                }
-            }
-            
-            // platform collision detection
-            platforms.forEach((platform)=> {
-            if(player.position.y + player.height <= platform.position.y &&
-               player.position.y + player.height + player.velocity.y >= platform.position.y && 
-               player.position.x + player.width >= platform.position.x && 
-               player.position.x <= platform.position.x + platform.width){
-              player.velocity.y = 0;
-            }
-          });
-
-          if(scrollOffset > 2000)
-          {
-            if(!gameOver){
-              gameOver = true
-              console.log('win');
-              
-            }
-          }
-
-          if(player.position.y > canvasState.height)
-          {
-            //newGame();    
-          }
-        }
-
-        if(platforms !== undefined)
-        {
-          platforms.forEach((platform)=>{
-            platform.draw();
-          })
-        }
-
-    } 
+ 
     
     const switchTile = (tile) => {
           switch(tile) {
@@ -194,7 +108,6 @@ const Engine = ({level}) => {
 
     const init = (canvas, ctx, level) => {
 
-        console.log('test');
         const levelPlatforms = level.map((row,rowId)=>(
           row.map((block,colId)=>{
             // console.log(`${block}-${rowId}:${colId}`);
@@ -222,6 +135,7 @@ const Engine = ({level}) => {
         setGenericObjects(genericObjects);
         setCtxState(ctx);
         setCanvasState(canvas);
+        p.update();
         return p;
         
     }
@@ -234,25 +148,118 @@ const Engine = ({level}) => {
         canvas.width= windowWidth;
         canvas.height= windowHeight;
 
-        const p = init(canvas, ctx, level);
-        
-         p.update();
+        init(canvas, ctx, level);
       });
     
-    const newGame = () => {
-      const canvas = canvasRef.current;
-      const ctx = canvasRef.current.getContext("2d");
-      const p = init(canvas, ctx, level);
-      p.update();
+    const newGame = (cRef) => {
+      console.log('new game', cRef);
+      const canvas = cRef.current;
+      const ctx = cRef.current.getContext("2d");
+      init(canvas, ctx, level)
     }
     
-    player && animate();
+    const animate = (gameOver) => {
+      const gameOver2 = gameOver;
+
+      if(player.position.y > canvasState.height)
+      {
+        if(!gameOver2)
+        {
+          console.log('raz', gameOver2);
+          gameOver2 = true;
+        }
+            
+      }
+      requestAnimationFrame(()=>animate(gameOver2));
+      if(ctxState && canvasState)
+      {
+          ctxState.fillStyle = "white";
+          ctxState.fillRect(
+              0,
+              0, 
+              canvasState.width, 
+              canvasState.height
+            );
+      }
+
+      if(genericObjects){
+        genericObjects.forEach(genericObject => {
+          genericObject.draw();
+        })
+      }
+
+      if(player !== undefined && platforms !== undefined){
+          player.update();
+          // console.log('a',isPressedKeyRight)
+          if(keys.right.pressed && player.position.x < 400) {
+              player.velocity.x = playerSpeed;
+          } else if(keys.left.pressed && player.position.x > 100) {
+              player.velocity.x = -playerSpeed;
+          } else {
+              player.velocity.x = 0;
+
+              if(keys.right.pressed){
+                scrollOffset += playerSpeed;
+
+                platforms.forEach((platform)=>{
+                  platform.position.x -= playerSpeed
+                })
+
+                genericObjects.forEach((genericObject)=>{
+                  genericObject.position.x -= playerSpeed * 0.66
+                })
+               
+              } else  if(keys.left.pressed){
+
+                platforms.forEach((platform)=>{
+                  scrollOffset -= playerSpeed;
+                  platform.position.x += playerSpeed
+                })
+
+                genericObjects.forEach((genericObject)=>{
+                  genericObject.position.x += playerSpeed * 0.66
+                })
+              }
+          }
+          
+          // platform collision detection
+          platforms.forEach((platform)=> {
+          if(player.position.y + player.height <= platform.position.y &&
+             player.position.y + player.height + player.velocity.y >= platform.position.y && 
+             player.position.x + player.width >= platform.position.x && 
+             player.position.x <= platform.position.x + platform.width){
+            player.velocity.y = 0;
+          }
+        });
+
+        if(scrollOffset > 2000)
+        {
+          if(!gameOver){
+            gameOver = true
+            console.log('win');
+            
+          }
+        }
+
+       
+      }
+
+      if(platforms !== undefined)
+      {
+        platforms.forEach((platform)=>{
+          platform.draw();
+        })
+      }
+
+  } 
+    console.log(gameOver);
+    player && animate(gameOver);
     return <>
         <Toaster
       position="top-center"
       reverseOrder={false}
     />
-    <button onClick={newGame}>start</button>
+    <button onClick={()=>newGame(canvasRef)}>start</button>
     <canvas ref={canvasRef}></canvas>
     </>
 }
